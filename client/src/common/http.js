b931eb8d2ab3ba13c53f53d21d541e19668168ed
minahuang <minahuang@jubo.health>
2021-12-26 14:34:17 +0800

    add react env variable

--- client/src/common/http.js
+++ client/src/common/http.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const rootUrl = process.env.baseURL || "http://localhost:3050";
+const rootUrl = process.env.REACT_APP_API_HOST || "http://localhost:3050";
+console.log("rootUrl", rootUrl);
 const rootApiUrl = `${rootUrl}/api/v1/`;
 export default axios.create({
   baseURL: rootApiUrl,
